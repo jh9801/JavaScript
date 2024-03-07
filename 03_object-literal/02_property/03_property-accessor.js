8052e903f17ad86ae51bb2d4d8e8eac085335873
hy9801 <wjdgksdud1649@gmail.com>
2024-03-07 17:49:42 +0900

    commit

--- 03_object-literal/02_property/03_property-accessor.js
+++ 03_object-literal/02_property/03_property-accessor.js
@@ -2,8 +2,8 @@
 
 
 var dog = {
-    name : '뽀삐',
-    eat : function(food){
+    name: '뽀삐',
+    eat: function (food) {
         console.log(`${this.name}(은)는 ${food}를 맛있게 먹어요.`)
     }
 };
@@ -18,8 +18,8 @@ console.log(dog['name']);
 dog['eat']('고구마');
 
 var obj = {
-    'dash-key' : 'dash-value',
-    0 : 1
+    'dash-key': 'dash-value',
+    0: 1
 
 };
 
